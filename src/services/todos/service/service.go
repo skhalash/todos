3c61e76bdcd4fe89c7f4eb4f966923b3cb599803
Stanislav <stanislav.khalash@gmail.com>
2019-03-06 21:29:35 +0100

    Set content type

--- src/services/todos/service/service.go
+++ src/services/todos/service/service.go
@@ -94,6 +94,7 @@ func (s Service) handleGetTodos(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Error marshaling json", http.StatusInternalServerError)
 	}
 
+	w.Header().Set("Content-Type", "application/json")
 	w.Write(bytes)
 }
 
