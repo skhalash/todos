9199edb52948ffd0ff9babbc13acb3853837aec1
Stanislav <stanislav.khalash@gmail.com>
2019-03-06 15:58:15 +0100

    Added a rand util pkg

---
+++ src/services/utils/rand/rand.go
@@ -0,0 +1,25 @@
+package rand
+
+import (
+	"math/rand"
+	"time"
+)
+
+const charset = "abcdefghijklmnopqrstuvwxyz" +
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+var seededRand = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+// String generates a random string of given length
+func String(length int) string {
+	return StringWithCharset(length, charset)
+}
+
+// StringWithCharset generates a random string of given length out of chars from given charset
+func StringWithCharset(length int, charset string) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
+}
